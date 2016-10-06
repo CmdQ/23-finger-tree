7d6663c18ccd500f6b0a3e7cfd965cc54576fc4b
Tobias Becker <rh-clash@web.de>
2016-10-06 14:42:05 +0200

    Be more positive! Silly change.

--- Tests/Program.fs
+++ Tests/Program.fs
@@ -1,7 +1,8 @@
-﻿open Fuchu
+﻿open CmdQ.Tests
+open Fuchu
 open FsCheck
 
 [<EntryPoint>]
 let main argv =
-    Arb.registerByType typeof<MyArbitraries.NegInt>.DeclaringType |> ignore
+    Arb.registerByType typeof<MyArbitraries.PosInt>.DeclaringType |> ignore
     Tests.defaultMainThisAssembly argv
