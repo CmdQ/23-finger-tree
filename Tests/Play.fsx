71d0e18035a75b27fe234700e12a321e2829df61
Tobias Becker <rh-clash@web.de>
2016-11-03 09:16:31 +0100

    Need script in test project.

---
+++ Tests/Play.fsx
@@ -0,0 +1,12 @@
+﻿#r @"..\FingerTree\bin\Debug\FingerTree.dll"
+
+open CmdQ
+
+let test = RandomAccess.ofList [1..10]
+test |> RandomAccess.toList |> printfn "%A"
+test |> RandomAccess.tryItem 0
+test |> RandomAccess.tryItem 5
+test |> RandomAccess.tryItem 9
+test |> RandomAccess.tryItem 10
+
+test |> FingerTree.butLast
