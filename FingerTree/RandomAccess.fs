fcf2e9639067d8d5d1ef2063ce06a87a589d8d1f
Tobias Becker <rh-clash@web.de>
2016-11-11 15:38:32 +0100

    @ Code doc

--- FingerTree/RandomAccess.fs
+++ FingerTree/RandomAccess.fs
@@ -5,29 +5,39 @@ open Monoids
 open Monoids.RandomAccess
 open CmdQ.FingerTree.Monoidal
 
+/// Functions for querying and manipulating random access finger trees.
 module RandomAccess =
+    /// A shorthand type for a finger tree.
     type Tree<'a> = FingerTree<Size, Value<'a>>
 
     let inline unpack (Value v) = v
 
+    /// The empty tree.
     let empty : Tree<_> = Empty
 
+    /// Checks whether a tree is empty.
     let isEmpty : Tree<_> -> bool = function
         | Empty -> true
         | _ -> false
 
-    let prepend value (tree:Tree<_>) = tree |> FingerTree.prepend (Value value)
-
+    /// Append an element to the right of a tree.
     let append value (tree:Tree<_>) = tree |> FingerTree.append (Value value)
+
+    /// Prepend an element to the left of a tree.
+    let prepend value (tree:Tree<_>) = tree |> FingerTree.prepend (Value value)
 
     let inline ofSomething f sth : Tree<_> = sth |> f (flip append) Empty
 
+    /// Create a finger tree from a sequence.
     let ofList list = list |> ofSomething List.fold
 
+    /// Create a finger tree from a list.
     let ofSeq seq = seq |> ofSomething Seq.fold
 
+    /// Create a finger tree from an array.
     let ofArray array = array |> ofSomething Array.fold
 
+    /// Convert a tree to a list (left to right).
     let toList tree =
         let rec toList acc (tree:Tree<_>) =
             match FingerTree.viewr tree with
@@ -36,10 +46,13 @@ module RandomAccess =
             | Nil -> acc
         toList [] tree
 
+    /// Convert a tree to a sequence, i.e. enumerate all elements left to right.
     let toSeq tree = tree |> (toList >> List.toSeq)
 
+    /// Return the number of elements in the tree.
     let length (tree:Tree<_>) = (fmeasure tree).Value
 
+    /// Convert a tree to an array (left to right).
     let toArray (tree:Tree<_>) =
         let len = length tree
         let re = Array.zeroCreate len
@@ -53,28 +66,34 @@ module RandomAccess =
         toArray 0 tree
         re
 
+    /// Concatenate two finger trees.
     let concat (left:Tree<_>) (right:Tree<_>) : Tree<_> = FingerTree.concat left right
 
+    /// Return the head of the finger tree (i.e. the left-most element).
     let head (tree:Tree<_>) =
         match FingerTree.viewl tree with
         | View(Value v, _) -> v
         | _ -> invalidArg "tree" Messages.treeIsEmpty
 
+    /// Return the tail of the tree, i.e. all but the first element.
     let tail (tree:Tree<_>) : Tree<_> =
         match FingerTree.viewl tree with
         | View(_, Lazy t) -> t
         | _ -> invalidArg "tree" Messages.treeIsEmpty
 
+    /// Return the last (i.e. the right-most) element of the finger tree.
     let last (tree:Tree<_>) =
         match FingerTree.viewr tree with
         | View(Value v, _) -> v
         | _ -> invalidArg "tree" Messages.treeIsEmpty
 
+    /// Return the spine of the tree, i.e. all but the last element.
     let butLast (tree:Tree<_>) : Tree<_> =
         match FingerTree.viewr tree with
         | View(_, Lazy t) -> t
         | _ -> invalidArg "tree" Messages.treeIsEmpty
 
+    /// Apply a mapping to a sequence and merge all resulting finger trees into one.
     let collect mapping = Seq.map mapping >> Seq.fold concat Empty
 
     let outsideError () = invalidIndex Messages.indexOutOfRange
@@ -99,18 +118,21 @@ module RandomAccess =
 
     let get tree index = item index tree
 
+    /// Return a new tree where a given position is replaced with a new value.
     let set tree index value : Tree<_> =
         indexChecked outsideError (fun (index, tree) ->
             let (Split(left, _, right)) = tree |> FingerTree.split (fun x -> x.Value > index) (Size())
             FingerTree.prepend (Value value) right |> FingerTree.concat left
         ) index tree
 
+    /// Remove an element at a given position from the tree.
     let removeIndex tree index : Tree<_> =
         indexChecked outsideError (fun (index, tree) ->
             let (Split(left, _, right)) = tree |> FingerTree.split (fun x -> x.Value > index) (Size())
             FingerTree.concat left right
         ) index tree
 
+    /// Return the depth of a tree while also checking that all measures are correct.
     let rec depthCheck<'a
         when 'a :> IMeasured<Size>
         > : FingerTree<Size, 'a> -> int = function
