f1057db120f15f8795f28636ca26fe73e27d745e
Tobias Becker <rh-clash@web.de>
2016-11-11 10:02:50 +0100

    Add total benchmark time output.

--- Tests/Program.fs
+++ Tests/Program.fs
@@ -1,12 +1,16 @@
 ﻿open CmdQ.FingerTree.Tests
 open Fuchu
 open FsCheck
+open System.Diagnostics
 
 [<EntryPoint>]
 let main argv =
     if Array.contains "--benchmark" argv then
         printfn "Benchmarking..."
+        let sw = Stopwatch.StartNew()
         Benchmark.benchmarks |> List.iter (fun b -> b())
+        sw.Stop()
+        printfn "\nBenchmarking took %.3f s in total." (float sw.ElapsedMilliseconds / 1000.0)
         0
     else
         printfn "Running tests..."
