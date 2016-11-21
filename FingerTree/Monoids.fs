b0df7d8ad4a4339a79a6c80af73d8108e0de6f64
Tobias Becker <rh-clash@web.de>
2016-11-21 11:25:53 +0100

    Add priority queue.

--- FingerTree/Monoids.fs
+++ FingerTree/Monoids.fs
@@ -1,5 +1,7 @@
 ﻿module CmdQ.FingerTree.Monoids
 
+open System
+
 /// Helper type to make any default-constructable type a singleton.
 type Singleton<'a when 'a : (new : unit -> 'a)> private() =
     static let instance = new 'a()
@@ -23,7 +25,7 @@ let mconcat monoids = monoids |> (List.map fmeasure >> List.reduce (fun a b -> a
 module RandomAccess =
     [<Sealed>]
     type Size(value) =
-        new() = Size 0
+        new () = Size 0
 
         member __.Value = value
 
@@ -42,3 +44,22 @@ module RandomAccess =
         override me.ToString () =
             match me with
             | Value v -> v.ToString()
+
+module PriorityQueue =
+    [<Sealed>]
+    type Priority(priority) =
+        new () = Priority(Single.NegativeInfinity)
+
+        member __.Priority = priority
+
+        interface IMonoid<Priority> with
+            member __.Zero = Priority()
+            member __.Plus rhs = Priority(max priority rhs.Priority)
+
+    type PriorityAndValue<'a> =
+        {
+            Priority : float32
+            Value : 'a
+        }
+        interface IMeasured<Priority> with
+            member me.Measure = Priority(me.Priority)
---
+++ FingerTree/PriorityQueue.fs
@@ -0,0 +1,28 @@
+﻿module CmdQ.FingerTree.PriorityQueue
+
+open CmdQ.FingerTree.Monoidal
+open Monoids
+open Monoids.PriorityQueue
+
+/// A shorthand type for a finger tree.
+type Tree<'a> = FingerTree<Priority, PriorityAndValue<'a>>
+
+let empty : Tree<_> = Empty
+
+/// Checks whether a tree is empty.
+let isEmpty : Tree<_> -> bool = function
+    | Empty -> true
+    | _ -> false
+
+let add item priority : Tree<_> -> Tree<_> = FingerTree.append { Priority = priority; Value = item }
+
+let inline ofSomething f sth : Tree<_> = sth |> f (fun acc (i, p) -> acc |> add i p) Empty
+
+/// Create a finger tree from a sequence.
+let ofList list = list |> ofSomething List.fold
+
+/// Create a finger tree from a list.
+let ofSeq seq = seq |> ofSomething Seq.fold
+
+/// Create a finger tree from an array.
+let ofArray array = array |> ofSomething Array.fold
